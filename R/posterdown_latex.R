70f8740177d7432543560a3b26f0d3293b3cbc1b
brentthorne <brent.thorne18@gmail.com>
2019-02-06 17:05:11 -0500

    Update to v0.1.2 with HTML support
    moved posterdown_pdf to posterdown_latex, added posterdown_html, added to ReadME, added ciation file. added @shedimus  as contributor in Description file.

---
+++ R/posterdown_latex.R
@@ -0,0 +1,33 @@
+#' Posterdown LaTeX format (using Memoir LaTeX document class)
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "posterdown_pdf", package = "posterdown")
+#' }
+#' @export
+# This is borrowed from the thesisdown package github.com/ismayc/thesisdown
+posterdown_latex <- function(..., keep_tex = TRUE) {
+  inherit_pdf_document(...,
+                       keep_tex = keep_tex,
+                       template = find_resource("posterdown_latex", "template.tex"),
+                       citation_package = "biblatex")
+}
+
+#' @description Legacy function for the OG's of posterdown.
+#' This will remain untouched from here on out (Feb 6 2019).
+#' @rdname posterdown_latex
+#' @export
+posterdown_pdf <- function(..., keep_tex = TRUE) {
+  inherit_pdf_document(...,
+                       keep_tex = keep_tex,
+                       template = find_resource("posterdown_pdf", "template.tex"),
+                       citation_package = "biblatex")
+}
