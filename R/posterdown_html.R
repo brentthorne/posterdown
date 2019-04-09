edd17768cf9230edbf8917aafdcf86ffe032bfe6
brentthorne <brent.thorne18@gmail.com>
2019-04-09 12:35:49 -0400

    add posterdown_betterport
    portrait template for better poster design has been added
    
    - support for longer author list if needed, can specify various authors to be highlited in the begining of the list.
    
    - better responsiveness to spacing when changinf poster size
    
    -  may need to go back and ensure YAML options amon all templates are more standardized.

--- R/posterdown_html.R
+++ R/posterdown_html.R
@@ -26,3 +26,12 @@ posterdown_betterland <- function(...,
                             css = NULL) {
   pagedown::poster_relaxed(..., css = css, template = template)
 }
+#' @description The output format \code{poster_betterport()} mimics the style of the
+#' \dQuote{BetterPoster} movement from twitter.
+#' @rdname posterdown_html
+#' @export
+posterdown_betterport <- function(...,
+                                  template = find_resource("posterdown_betterport", "template.html"),
+                                  css = NULL) {
+  pagedown::poster_relaxed(..., css = css, template = template)
+}
